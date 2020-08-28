ed0640b3894559b3db99bd11d67dc82f188b5e46
Sergey Grinko <s.m.grinko@icloud.com>
2020-08-28 17:39:55 +0300

    Initial Commit

---
+++ Data Warehouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Data Warehouse
+//
+//  Created by Сергей Гринько on 28.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
